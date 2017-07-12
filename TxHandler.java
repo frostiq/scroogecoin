073cee7ef55b3b19a9e6ed785f87512937e6e855
Aliaksandr Bazhanau <a.bazhanau@adform.com>
2017-07-13 00:09:42 +0300

    isValidTx implemented

--- TxHandler.java
+++ TxHandler.java
@@ -13,15 +13,19 @@ public class TxHandler {
 
     /**
      * @return true if:
-     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
-     * (2) the signatures on each input of {@code tx} are valid, 
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool,
+     * (2) the signatures on each input of {@code tx} are valid,
      * (3) no UTXO is claimed multiple times by {@code tx},
      * (4) all of {@code tx}s output values are non-negative, and
      * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
-     *     values; and false otherwise.
+     * values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        // IMPLEMENT THIS
+        return allClaimedOutputsInPool(tx) &&
+                signaturesAreValid(tx) &&
+                noDoubleSpend(tx) &&
+                allOutputsAreNonNegative(tx) &&
+                sumsIsCorrect(tx);
     }
 
     /**
@@ -30,7 +34,54 @@ public class TxHandler {
      * updating the current UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
-        // IMPLEMENT THIS
+        return possibleTxs;
     }
 
+    private boolean allClaimedOutputsInPool(Transaction tx) {
+        return tx.getInputs().stream().allMatch(input ->
+                _pool.contains(new UTXO(input.prevTxHash, input.outputIndex))
+        );
+    }
+
+    private boolean signaturesAreValid(Transaction tx) {
+        for (int i = 0; i < tx.numInputs(); i++) {
+            Transaction.Input input = tx.getInput(i);
+            Transaction.Output output = _pool.getTxOutput(new UTXO(
+                    input.prevTxHash,
+                    input.outputIndex
+            ));
+            boolean isValid = Crypto.verifySignature(
+                    output.address,
+                    tx.getRawDataToSign(i),
+                    input.signature
+            );
+            if(!isValid)
+                return false;
+        }
+        return true;
+    }
+
+    private boolean noDoubleSpend(Transaction tx) {
+        long utxoCount = tx.getInputs().stream()
+                .map(x -> new UTXO(x.prevTxHash, x.outputIndex))
+                .distinct()
+                .count();
+
+        return utxoCount == tx.numInputs();
+    }
+
+    private boolean allOutputsAreNonNegative(Transaction tx) {
+        return tx.getOutputs().stream().allMatch(output -> output.value >= 0);
+    }
+
+    private boolean sumsIsCorrect(Transaction tx) {
+        double inputSum = tx.getInputs().stream()
+                .mapToDouble(x -> _pool.getTxOutput(new UTXO(x.prevTxHash, x.outputIndex)).value)
+                .sum();
+        double outputSum = tx.getOutputs().stream()
+                .mapToDouble(x -> x.value)
+                .sum();
+
+        return inputSum >= outputSum;
+    }
 }
